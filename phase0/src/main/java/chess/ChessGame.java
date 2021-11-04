35f871ce8013b1e52dc7c45fe8570986c2dfb8e4
liang29 <wanglong.email@foxmail.com>
2021-11-05 01:06:48 +0800

    Add player classes that can be persisted

--- phase0/src/main/java/chess/ChessGame.java
+++ phase0/src/main/java/chess/ChessGame.java
@@ -26,17 +26,46 @@ public class ChessGame {
         Player player2 = PlayerFactory.newPlayer("demo2", PlayerRole.Gust);
         Chessboard board = new Chessboard(8, 8);
 
-        PieceRole[] pieceRoles = {Rook, Knight, Bishop, Queen, King, Bishop, Knight, Rook};
-        for (int i = 0; i < 8; i++) {
-            // White pawns
-            board.tiles[i][1].setCurrentPiece(PieceFactory.newPiece(Pawn, "white", i, 1));
-            // Black pawns
-            board.tiles[i][6].setCurrentPiece(PieceFactory.newPiece(Pawn, "black", i, 6));
-            // Other white pieces
-            board.tiles[i][0].setCurrentPiece(PieceFactory.newPiece(pieceRoles[i],"white", i, 0));
-            // Other black pieces
-            board.tiles[i][7].setCurrentPiece(PieceFactory.newPiece(pieceRoles[i],"black", i, 7));
-        }
+        // White pawns
+        board.tiles[0][1].setCurrentPiece(new Pawn("white", 0, 1));
+        board.tiles[1][1].setCurrentPiece(new Pawn("white", 1, 1));
+        board.tiles[2][1].setCurrentPiece(new Pawn("white", 2, 1));
+        board.tiles[3][1].setCurrentPiece(new Pawn("white", 3, 1));
+        board.tiles[4][1].setCurrentPiece(new Pawn("white", 4, 1));
+        board.tiles[5][1].setCurrentPiece(new Pawn("white", 5, 1));
+        board.tiles[6][1].setCurrentPiece(new Pawn("white", 6, 1));
+        board.tiles[7][1].setCurrentPiece(new Pawn("white", 7, 1));
+
+        // Black pawns
+        board.tiles[0][6].setCurrentPiece(new Pawn("black", 0, 6));
+        board.tiles[1][6].setCurrentPiece(new Pawn("black", 1, 6));
+        board.tiles[2][6].setCurrentPiece(new Pawn("black", 2, 6));
+        board.tiles[3][6].setCurrentPiece(new Pawn("black", 3, 6));
+        board.tiles[4][6].setCurrentPiece(new Pawn("black", 4, 6));
+        board.tiles[5][6].setCurrentPiece(new Pawn("black", 5, 6));
+        board.tiles[6][6].setCurrentPiece(new Pawn("black", 6, 6));
+        board.tiles[7][6].setCurrentPiece(new Pawn("black", 7, 6));
+
+        // Other white pieces
+        board.tiles[0][0].setCurrentPiece(new Rook("white", 0, 0));
+        board.tiles[1][0].setCurrentPiece(new Knight("white", 1, 0));
+        board.tiles[2][0].setCurrentPiece(new Bishop("white", 2, 0));
+        board.tiles[3][0].setCurrentPiece(new Queen("white", 3, 0));
+        board.tiles[4][0].setCurrentPiece(new King("white", 4, 0));
+        board.tiles[5][0].setCurrentPiece(new Bishop("white", 5, 0));
+        board.tiles[6][0].setCurrentPiece(new Knight("white", 6, 0));
+        board.tiles[7][0].setCurrentPiece(new Rook("white", 7, 0));
+
+        // Other black pieces
+        board.tiles[0][7].setCurrentPiece(new Rook("black", 0, 7));
+        board.tiles[1][7].setCurrentPiece(new Knight("black", 1, 7));
+        board.tiles[2][7].setCurrentPiece(new Bishop("black", 2, 7));
+        board.tiles[3][7].setCurrentPiece(new Queen("black", 3, 7));
+        board.tiles[4][7].setCurrentPiece(new King("black", 4, 7));
+        board.tiles[5][7].setCurrentPiece(new Bishop("black", 5, 7));
+        board.tiles[6][7].setCurrentPiece(new Knight("black", 6, 7));
+        board.tiles[7][7].setCurrentPiece(new Rook("black", 7, 7));
+
         return new ChessGame(player1, player2, board);
     }
 
