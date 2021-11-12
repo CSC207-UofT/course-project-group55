3b1d1260409beebbec0f6e1874b94588bde05b96
kmgiwonkim <45646279+kmgiwonkim@users.noreply.github.com>
2021-11-12 16:59:30 -0500

    Minor Change in ChessTurn

--- phase0/src/main/java/chess/ChessTurn.java
+++ phase0/src/main/java/chess/ChessTurn.java
@@ -3,13 +3,11 @@ package chess;
 public class ChessTurn {
     final Coord moveFrom;
     final Coord moveTo;
-    final boolean isTake;
     final boolean isCheck;
 
     public ChessTurn(Coord from, Coord to, boolean check){
         this.moveFrom = from;
         this.moveTo = to;
-        isTake = take;
         isCheck = check;
     }
 
