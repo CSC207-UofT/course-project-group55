836c6aabef5114bf2965f97ae3472e1f3e50775c
johnwon2007 <johnwon2007@gmail.com>
2021-11-26 22:30:01 -0500

    Working Methods For LAN

---
+++ phase0/src/main/java/lan/Client.java
@@ -0,0 +1,83 @@
+package lan;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+    private Socket socket;
+    private BufferedReader bufferedReader;
+    private BufferedWriter bufferedWriter;
+    private String username;
+
+    public Client(Socket socket, String username) throws IOException {
+        this.socket = socket;
+        this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        this.username = username;
+    }
+
+    public void sendMessage(String move) throws IOException {
+        bufferedWriter.write(this.username + " " + move);
+        bufferedWriter.newLine();
+        bufferedWriter.flush();
+    }
+
+    //Used to End Close Server
+    public void sendGameOver() throws IOException {
+        bufferedWriter.write("GameOver");
+        bufferedWriter.newLine();
+        bufferedWriter.flush();
+    }
+
+    public String receiveMessage() throws IOException {
+        return bufferedReader.readLine();
+    }
+
+    public void closeSocketBuffered() {
+        try{
+            //closing bufferedReader and Writer will close outputStreamWriter and InputStreamReader
+            if (this.bufferedReader != null){
+                this.bufferedReader.close();
+            }
+            if (this.bufferedWriter != null){
+                this.bufferedWriter.close();
+            }
+            //closing socket will close socket.InputStream and socket.OutputStream
+            if (this.socket != null){
+                this.socket.close();
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    //Testing main
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your username for the group chat: ");
+        String username = scanner.nextLine();
+        //connecting to the server port 1234
+        Socket socket = new Socket("localhost", 1234);
+        Client client = new Client(socket, username);
+        client.sendMessage("");
+        String playersName = client.receiveMessage();
+        System.out.println("Players are " + playersName);
+
+        String[] names = playersName.split(" ");
+        if (names[0].equals(client.username)){
+            System.out.println("Type Message: ");
+            client.sendMessage(scanner.nextLine());
+            System.out.println(client.receiveMessage());
+        }
+        else{
+            System.out.println(client.receiveMessage());
+            client.sendMessage(scanner.nextLine());
+        }
+        client.closeSocketBuffered();
+    }
+
+}
+
+
+
---
+++ phase0/src/main/java/lan/Server.java
@@ -0,0 +1,75 @@
+package lan;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class Server {
+    ServerSocket serverSocket = null;
+    ArrayList<ClientHandler> clientHandlers = new ArrayList<>();
+
+    //Runs Server with numPlayers allowed at given port.
+    //TODO: Later work on if there already exists port number
+    public void runServer(int numPlayers, int port) throws IOException {
+        Socket socket = null;
+        this.serverSocket = new ServerSocket(port);
+        String playerNames = "";
+        ClientHandler clientHandler;
+        //Get numPlayer's info
+        for (int i = 0; i < numPlayers; i++) {
+            socket = this.serverSocket.accept();
+            clientHandler = new ClientHandler(socket);
+            //System.out.println(clientHandler.clientUsername);
+            clientHandlers.add(clientHandler);
+        }
+        //can close server because all connections are made.
+        closeServer();
+        for (ClientHandler client : clientHandlers) {
+            playerNames += " " + client.clientUsername;
+        }
+        messageToAll(playerNames.strip());
+        int i = 0;
+        String received = "";
+        //loops until GameOver is sent by the Game
+        while(!received.equals("GameOver")){
+            received = clientHandlers.get(i % numPlayers).receiveMessage();
+            broadcastMessage(received);
+            i++;
+        }
+    }
+
+    //Closes Server
+    public void closeServer() throws IOException {
+        if (this.serverSocket != null){
+            this.serverSocket.close();
+        }
+    }
+    //Sends Message to all clients in the Server
+    public void messageToAll(String message) throws IOException {
+        for (ClientHandler clientHandler : clientHandlers) {
+            clientHandler.sendMessage(message);
+        }
+    }
+
+    //sends Message to all the players other then client itself.
+    public void broadcastMessage(String message) throws IOException {
+        String sentby = message.split(" ")[0];
+        for (ClientHandler clientHandler : clientHandlers) {
+            //System.out.println(clientHandler.clientUsername + "'s Turn to receive message");
+            //System.out.println("The Message is sent by : " + sentby + " and received by " + clientHandler.clientUsername);
+            if(!sentby.equals(clientHandler.clientUsername)){
+                clientHandler.sendMessage(message);
+            }
+        }
+    }
+
+    //testing Main
+    public static void main(String[] args) throws IOException {
+        Server server = new Server();
+        server.runServer(2, 1234);
+    }
+
+}
+
+
